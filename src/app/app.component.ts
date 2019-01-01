fcb37cb7b0197f5d66ee6b7312849507ae2b407d
Amitha <amitha4g@gmail.com>
2019-01-01 16:49:55 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import * as d3 from "d3";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
   title = 'app';
+  @ViewChild('d3Container') nameInputRef: ElementRef;
+  barChartData = [30, 86, 168, 250, 310, 368];
+
+  constructor() {
+  }
+
+  genarateBarChart() {
+
+    console.log(this.nameInputRef.nativeElement);
+    d3.select(this.nameInputRef.nativeElement)
+      .selectAll("div")
+      .data(this.barChartData)
+      .enter()
+      .append("div")
+      // .attr("class", "chart")
+      .style("width", function (d) { return d + "px"; })
+      .style("font", "10px sans-serif")
+      .style("background-color", "steelblue")
+      .style("text-align", "right")
+      .style("padding", "3px")
+      .style("margin", "1px")
+      .style("color", "white")
+      .text(function (d) { return d; });
+  }
 }
